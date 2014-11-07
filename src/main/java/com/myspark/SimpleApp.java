18d3280919dd729cb26f74b2de6e2b85849a18f8
Terry <jie.shi@hp.com>
2014-11-07 17:41:31 +0800

    Analysis SimpleApp

--- src/main/java/com/myspark/SimpleApp.java
+++ src/main/java/com/myspark/SimpleApp.java
@@ -3,20 +3,37 @@ import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 
+
+/**
+ * Word Count
+ * @author shijie
+ *
+ */
 public class SimpleApp {
     public static void main(String[] args) {
         String logFile = "README.md"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application");
         conf.setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> logData = sc.textFile(logFile).cache(); // How many RDD operations ?
+        JavaRDD<String> logData = sc.textFile(logFile).cache(); // QUESTION: How many RDD operations ? what's the difference of using cache() ?
 
+        System.out.println("Total lines: " + logData.count()); // exclude the last empty line
+        
+        // RDD operation: RDD.filter(Function) ?   Input parameter is one line from RDD file.
+        // Return value of filter() method ? a RDD file which contains boolean value
         long numAs = logData.filter(new Function<String, Boolean>() {
             public Boolean call(String s) {
                 return s.contains("a");
             }
-        }).count();
+        }).count(); // count() is the totally number of lines
 
+        // save the line which contains "a"
+        logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) {
+                return s.contains("a");
+            }
+        }).saveAsTextFile("data/fileContainsA.txt");
+        
         long numBs = logData.filter(new Function<String, Boolean>() {
             public Boolean call(String s) {
                 return s.contains("b");
